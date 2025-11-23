98e916bd7b5077a7cb03d57e7253744afa96d731
Babbu-exe <umarmunafahmed@gmail.com>
2025-11-24 01:53:19 +0530

    arithmetic.c
    learned some arithematic stuff here ... too basic

---
+++ arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x=12;
+    int y=6;
+    int z=0;
+    printf("z=%d\n",x+y);
+    printf("z=%d\n",x-y);
+    printf("z=%d\n",x*y);
+    printf("z=%d\n",x%y);
+    printf("x++=x+1=%d\n",x+1);
+    printf("x--=x-1=%d\n",x-1);
+
+    return 0;
+}
