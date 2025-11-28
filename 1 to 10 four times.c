cff9f6cbdaa5e6e8b979d077c22792ea235d3b76
Babbu-exe <umarmunafahmed@gmail.com>
2025-11-28 21:40:31 +0530

    Add program to print numbers 1 to 10 four times

---
+++ 1 to 10 four times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i;
+    for(int i=0;i<4;i++){
+        for(int j=1;j<=10;j++)
+        {
+        printf("%d",j);
+        }
+     printf("\n");
+    }
+    return 0;
+}
