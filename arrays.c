cb931652863e0580158bec0fcdad1a89a757fa81
Babbu-exe <umarmunafahmed@gmail.com>
2025-12-01 01:34:01 +0530

    storing value in array
    Removed commented-out code related to prompts and learning.

---
+++ arrays.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int number[]={1,2,3,4,5,6};
+    char grades[]={'a','b','c','d','f'};
+    char name[]={"babbu"};
+    number[2]=30;
+    number[3]=40;
+    number[4]=50;
+    printf("%d\n", number[4]);
+    printf("%c\n",grades [2]);
+    printf("%c",name [4]);
+    return 0;
+}
+
+
+//prompt and learning how to store value in a array //
+#include <stdio.h>
+
+int main() {
+    int number[5]={0};
+    printf("coco:");
+    scanf("%d",&number[0]);
+    
+    printf("coco:");
+    scanf("%d",&number[1]);
+    
+    printf("coco:");
+    scanf("%d",&number[2]);
+    
+    printf("coco:");
+    scanf("%d",&number[3]);
+    
+    printf("coco:");
+    scanf(" %d",&number[4]);
+    
+    for(int i=0;i<5;i++){
+        printf("%d",number[i]);
+    }
+    printf("");
+    return 0;
+}    
