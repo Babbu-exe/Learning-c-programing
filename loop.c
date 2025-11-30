2e91cc5c2028bb5f5de5717a782ec7fb4009e4da
Babbu-exe <umarmunafahmed@gmail.com>
2025-11-30 23:49:05 +0530

    Implement reverse ladder printing in loop.c
    Added a program to print a reverse ladder in integer form based on user input for the number of rows.

--- loop.c
+++ loop.c
@@ -98,3 +98,21 @@ int main() {
 
     return 0;
 }
+
+
+
+//printing reverse ladder in intiger form //
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the no of rows: ");
+    scanf("%d",& n);
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n+1-i;j++){
+            printf("%d",j);
+        }
+    printf("\n");    
+    }
+    return 0;
+}
