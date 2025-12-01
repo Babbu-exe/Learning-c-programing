ec4914e504c713e71cea6bcc10e486e75e7210e5
Babbu-exe <umarmunafahmed@gmail.com>
2025-12-02 00:09:15 +0530

    Implement user input and display for 5x5 matrix
    Added functionality to take user input for a 5x5 matrix and print it.

--- arrays.c
+++ arrays.c
@@ -64,3 +64,25 @@ int main() {
     
     return 0;
 }
+
+
+// high level array shit ... taking user input for 5x5 matix and saving that in an array then printing that matix// 
+
+#include <stdio.h>
+int main (){
+    int a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0; j<5; j++){
+            scanf("%d",&a[i][j]);
+            }
+        printf("\n");
+        }
+    for (int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
